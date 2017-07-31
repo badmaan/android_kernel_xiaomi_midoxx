8612321edf50aebd5da6b81c8d83d91591cbde95
Jairus John <Jairus980@gmail.com>
2017-07-31 17:13:16 +0800

    remove sdcardfs unnecessary lock

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -680,13 +680,9 @@ static int sdcardfs_setattr(struct dentry *dentry, struct iattr *ia)
 	 * afterwards in the other cases: we fsstack_copy_inode_size from
 	 * the lower level.
 	 */
-	if (current->mm)
-		down_write(&current->mm->mmap_sem);
 	if (ia->ia_valid & ATTR_SIZE) {
 		err = inode_newsize_ok(inode, ia->ia_size);
 		if (err) {
-			if (current->mm)
-				up_write(&current->mm->mmap_sem);
 			goto out;
 		}
 		truncate_setsize(inode, ia->ia_size);
@@ -709,8 +705,6 @@ static int sdcardfs_setattr(struct dentry *dentry, struct iattr *ia)
 	err = notify_change(lower_dentry, &lower_ia, /* note: lower_ia */
 			NULL);
 	mutex_unlock(&lower_dentry->d_inode->i_mutex);
-	if (current->mm)
-		up_write(&current->mm->mmap_sem);
 	if (err)
 		goto out;
 
